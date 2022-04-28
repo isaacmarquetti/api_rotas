76b6426b409c15ddd4ed14e83a53c9880148bbc5
isaacmarquetti <isaac.marquetti@gmail.com>
2022-04-27 21:45:19 -0300

    Implementando a integração contínua.
    close #4

--- rotas/tests.py
+++ rotas/tests.py
@@ -1,3 +1,7 @@
-from django.test import TestCase
+from django.test import Client
 
-# Create your tests here.
+
+def test_home_status_code(client: Client):
+    resposta = client.get('/')
+    assert resposta.status_code == 200
+
