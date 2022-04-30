15dca89746d8545e97079df0928897f51077c54b
isaacmarquetti <isaac.marquetti@gmail.com>
2022-04-30 11:28:19 -0300

    Instalando Django Rest Framework
    - Alterando models, serializer, views e urls do projeto
    
    close #7

--- rotas/api/serializers.py
+++
@@ -1,9 +0,0 @@
-from rest_framework import serializers
-from rotas import models
-
-
-class RotaSerializer(serializers.ModelSerializer):
-
-    class Meta:
-        model = models.Rota
-        fields = '__all__'
