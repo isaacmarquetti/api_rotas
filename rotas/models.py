b611551991f0c019d4005e20e8dbe990a02edac6
isaacmarquetti <isaac.marquetti@gmail.com>
2022-05-01 22:25:22 -0300

    Adicionando testes close #10

--- rotas/models.py
+++ rotas/models.py
@@ -2,13 +2,26 @@ from django.db import models
 
 
 class Local(models.Model):
+    """
+    Método para atribuir um local como exemplo.
+    """
     local = models.CharField(max_length=30)
 
     def __str__(self):
         return self.local
 
+    class Meta:
+        verbose_name = 'Local'
+        verbose_name_plural = 'Locais'
+
 
 class Rota(models.Model):
+    """
+    Método para criar rotas entre os locais criados como exemplo.
+
+    Ao definir um local de origem "source" e um ponto de chegada "target",
+    é atribuido uma distância entre dois pontos em uma rota unidirecional.
+    """
     Town = (
         ('A', 'Exemplo A'),
         ('B', 'Exemplo B'),
@@ -23,3 +36,6 @@ class Rota(models.Model):
     def __str__(self):
         return self.source
 
+    class Meta:
+        verbose_name = 'Rota'
+        verbose_name_plural = 'Rotas'
--- rotas/tests.py
+++ rotas/tests.py
@@ -1,7 +1,54 @@
 from django.test import Client
+import requests
 
 
 def test_home_status_code(client: Client):
+    """
+    Teste status code 200 (OK), na página inicial.
+    """
     resposta = client.get('/')
     assert resposta.status_code == 200
 
+
+class TestLocal:
+    url_base_local = 'http://127.0.0.1:8000/api/v1/local/'
+
+    def test_get_locais(self):
+        """
+        Teste status code 200 (OK) para todos os locais.
+        """
+        locais = requests.get(url=self.url_base_local)
+        assert locais.status_code == 200
+
+    def test_get_local(self):
+        """
+        Teste status code 200 (OK) para um local específico.
+        """
+        local = requests.get(url=f'{self.url_base_local}1/')
+        assert local.status_code == 200
+
+    def test_get_registros(self):
+        """
+        Teste para confirmar 5 locais como exemplo.
+        """
+        registros = requests.get(url=self.url_base_local)
+        assert registros.json()[-1]['id'] == 5
+
+    def test_get_local_invalido(self):
+        """
+        Teste para confirmar que não há dados no id=6, retornando
+        o erro 404 (Página não encontrada).
+        """
+        local = requests.get(url=f'{self.url_base_local}6/')
+        assert local.status_code == 404
+
+
+class TestRota:
+    url_base_local = 'http://127.0.0.1:8000/api/v1/rota/'
+
+    def test_get_rotas(self):
+        """
+        Teste status code 200 (OK) para todas as rotas.
+        """
+        rotas = requests.get(url=self.url_base_local)
+        assert rotas.status_code == 200
